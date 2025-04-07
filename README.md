dc6fa8a7fd639f7cde4f43c12bdc78c2d0dad983
hemant087 <88886834+hemant087@users.noreply.github.com>
2025-04-07 18:17:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 30Days_EDA
+ This will be going to be a 30 EDA projects, while learning
---
+++ demo.py
@@ -0,0 +1,6 @@
+def func(num):
+    '''This function is created by hemant to test the libreary import'''
+    if num%2 == 0:
+        return "EVEN"
+    else:
+        return "ODD"
